8b20a92f1cd537f84e7623817f3702d515dd4b35
yousif30303 <yousif30303@gmail.com>
2021-04-02 16:53:11 -0700

    adding users sign up code

---
+++ Sign-up.php
@@ -0,0 +1,29 @@
+<?php
+ // Headers
+ header('Access-Control-Allow-Origin: *');
+ header('Content-Type: application/json');
+ header('Access-Control-Allow-Methods: POST');
+ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+//import database file
+include('connection.php');
+
+//get the data from json
+$data = json_decode(file_get_contents("php://input"));
+
+$first_name = $data->first_name;
+$last_name = $data->last_name;
+$email = $data->email;
+$password = $data->password;
+$Role = $data->Role;
+
+//insert the data into the table
+$sql = "INSERT INTO users (`first_name`, `last_name`, `email`, `password`, `Role`) VALUES ('$first_name', '$last_name', '$email', '$password', '$Role')";
+
+//get the 
+$result = mysqli_query($link, $sql);
+if(!$result){
+    echo '<div class="alert alert-danger">There was an error inserting the users details in the database!</div>'; 
+    exit;
+}
+?>
